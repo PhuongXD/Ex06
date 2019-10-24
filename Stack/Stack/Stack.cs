8cbe6c11bf4ba8483548127aec639b0a5e10ce2b
PhuongXD <lanphuong2899@gmail.com>
2019-10-24 17:52:56 +0700

    Stack

---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abcd
+{
+    public class Stack
+    {
+        private string []Buffer;
+        private int Top;
+        public Stack (int size)
+        {
+            Buffer = new string[size];
+            Top = -1;
+        }
+        //buffer 1000 items
+        public Stack()
+        {
+            Buffer = new string[1000];
+            Top = -1;
+        }
+        public void Push(string v)
+        {
+            Buffer[Top+1] = v;
+            Top++;
+        }
+        public string Pop()
+        {
+            string v = Buffer[Top];
+            Top = Top - 1;
+            return v;
+        }
+        public string Peek()
+        {
+            return Buffer[Top];
+        }
+        public int Count()
+        {
+            return Top + 1;
+        }
+        public void Clear()
+        {
+            Top = -1;
+        }
+    }
+}
---
+++ Stack/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Abcd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPush()
+        {
+            Stack s = new Stack();
+            s.Push("a");
+            s.Push("u");
+            s.Push("o");
+            Assert.AreEqual("o", s.Count());
+            Assert.AreEqual("u", s.Pop());
+        }
+
+        [TestMethod]
+        public void TestPop()
+        {
+            Stack s = new Stack();
+            s.Push("a");
+            s.Push("u");
+            s.Push("o");
+            Assert.AreEqual("o", s.Pop());
+            Assert.AreEqual("u", s.Pop());
+        }
+        [TestMethod]
+        public void TestPeek()
+        {
+            Stack s = new Stack();
+            s.Push("a");
+            s.Push("u");
+            s.Push("o");
+            Assert.AreEqual("o", s.Peek());
+            Assert.AreEqual("u", s.Count());
+        }
+        [TestMethod]
+        public void TestClear()
+        {
+            Stack s = new Stack();
+            s.Push("a");
+            s.Push("u");
+            s.Push("o");
+            Assert.AreEqual("o", s.Peek());
+            Assert.AreEqual("u", s.Count());
+        }
+    }
+}
